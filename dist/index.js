10b2dfbe6e992eaf2394e14f54db009e82fc3a8b
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-05-19 17:37:16 -0300

    TS_Constructor

--- dist/index.js
+++ dist/index.js
@@ -2,14 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class bankAccount {
     accNumber;
+    funds;
+    constructor(accNumber, initialFunds) {
+        this.accNumber = accNumber;
+        this.funds = initialFunds;
+    }
 }
 function main() {
-    const account1 = new bankAccount();
-    const account2 = new bankAccount();
-    account1.accNumber = 123456789;
-    account2.accNumber = 987654321;
+    const account1 = new bankAccount(123456789, 1000);
+    const account2 = new bankAccount(987654321, 2000);
+    const account3 = new bankAccount(98541, 40000);
     console.log(`Account 1 Number: ${account1.accNumber}`);
     console.log(`Account 2 Number: ${account2.accNumber}`);
+    console.log(`Account 3 Number: ${account3.accNumber}`);
 }
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 class bankAccount {
-    accNumber: number | undefined;
+    accNumber: number;
+    funds: number;
+
+    constructor(accNumber: number, initialFunds: number) {
+        this.accNumber = accNumber;
+        this.funds = initialFunds;
+    }
 }
 
 function main() {
-    const account1 = new bankAccount();
-    const account2 = new bankAccount();
-
-    account1.accNumber = 123456789;
-    account2.accNumber = 987654321;
+    const account1 = new bankAccount(123456789, 1000);
+    const account2 = new bankAccount(987654321, 2000);
+    const account3 = new bankAccount(98541, 40000);
 
     console.log(`Account 1 Number: ${account1.accNumber}`);
     console.log(`Account 2 Number: ${account2.accNumber}`);
+    console.log(`Account 3 Number: ${account3.accNumber}`);
 }
 
 main ()
