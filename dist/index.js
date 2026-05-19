fb8c55e2ef8507d66b2083b1be100561f46952eb
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-05-19 16:30:24 -0300

    TS_EstruturaClass_01

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class bankAccount {
-    accNumber: number | undefined;
+    accNumber;
 }
-
 function main() {
     const account1 = new bankAccount();
     const account2 = new bankAccount();
-
     account1.accNumber = 123456789;
     account2.accNumber = 987654321;
-
     console.log(`Account 1 Number: ${account1.accNumber}`);
     console.log(`Account 2 Number: ${account2.accNumber}`);
 }
-
-main ()
+main();
+//# sourceMappingURL=index.js.map
