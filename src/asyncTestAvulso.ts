7b694f2cf19dba1be4f2cd56889e116c4e8ebbf9
Matheus Stopinski <matheus.stopinski@hotmail.com>
2026-05-23 23:36:13 -0300

    TS_setImmediate

--- src/asyncTestAvulso.ts
+++ src/asyncTestAvulso.ts
@@ -5,20 +5,24 @@ export function log(msg: string) {
 
 export function runAsyncTest(): void {
 
-log('Iniciando processamento...');
+setImmediate(() => {
+    log('setImmediate'); // terceira mensagem, pois é o setImmediate, que tem prioridade sobre o setTimeout
+});
+
+log('Iniciando processamento...'); // primeira mensagem pois é o regular
 
 setTimeout(() => {
-    log('setTimeout');
+    log('setTimeout'); // quarta mensagem, pois é o setTimeout, o mais demorado!
 }, 2000);
 
 setTimeout(() => {
-    log('setTimeout');
+    log('setTimeout'); // quinta mensagem, setTimeout seguindo a ordem!
 }, 4000);
 
 setTimeout(() => {
-    log('setTimeout');
+    log('setTimeout'); // sexta mensagem, setTimeout seguindo a ordem!
 }, 7000);
 
-log('Enquanto aguardou setTimeout, ele veio primeiro.');
+log('Enquanto aguardou setTimeout, ele veio primeiro.'); // segunda mensagem, pois é o regular, apenas seguindo a ordem!
 
 };
